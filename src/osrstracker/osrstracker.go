9faa1910804bb872e2db57a804581e740f132e00
Kallu609 <khirvola@gmail.com>
2019-01-31 20:41:49 +0200

    highscore fetch

--- src/osrstracker/osrstracker.go
+++ src/osrstracker/osrstracker.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// GetHighscores fetch player highscores
+func GetHighscores(playerName string) {
+	resp, err := http.Get("https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + playerName)
+
+	if err != nil {
+		// we are banned
+	}
+
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		str := strings.Trim(scanner.Text(), " \n")
+		tokens := strings.Split(str, ",")
+
+		if len(tokens) != 3 {
+			continue
+		}
+
+		fmt.Printf(scanner.Text() + "\n")
+	}
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	GetHighscores("aran penaali")
 }
